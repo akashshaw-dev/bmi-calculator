8b99b95e858b0beff4dedc69ec48de5a30a0f946
Akash Shaw <shawakash25274@gmail.com>
2025-12-29 18:14:28 +0530

    Enhance BMI calculator with validation, reset button, and UX improvements

--- script.js
+++ script.js
@@ -7,12 +7,27 @@ let btn = document.querySelector("#btn");
 let bmi = document.querySelector("#bmi");
 let category = document.querySelector("#category");
 
+let reset = document.querySelector("#resetBtn");
+
+reset.classList.add("disabled-effect");
+
 
 // Logic of BMI
 
 btn.addEventListener("click" , function(){
     let height_val = parseFloat(height.value);
     let weight_val = parseFloat(weight.value);
+
+
+    if(isNaN(height_val) || isNaN(weight_val) || weight_val <= 0 || height_val <= 0){
+        alert("Please enter valid numbers.");
+        reset.disabled = true;
+        reset.classList.add("disabled-effect");
+        bmi.textContent = "";
+        category.textContent = "";
+        return;
+    }
+
 
     let result = 0;
 
@@ -22,6 +37,8 @@ btn.addEventListener("click" , function(){
 
     bmi.textContent = `BMI: ${formattedBMI}`;
 
+
+    
 
     if(result < 18.5){
         category.textContent = `Category: Underweight`;
@@ -38,13 +55,24 @@ btn.addEventListener("click" , function(){
     else if(result <= 39.9){
         category.textContent = `Category: Obesity Class II`;
     }
-    else {
+    else if(result >= 40){
         category.textContent = `Category: Severe obesity`;
     }
+
+    reset.disabled = false;
+    reset.classList.remove("disabled-effect");
 
 });
 
 
+reset.addEventListener("click" , function(){
+    height.value = "";
+    weight.value = "";
+    bmi.textContent = "";
+    category.textContent = "";
+    reset.disabled = true;
+    reset.classList.add("disabled-effect");
+});
 
 
 
